3bf15ae63f8507aaab23a0135a59779af1595aa7
dpoeira <diogopoeira91@hotmail.com>
2020-10-25 21:13:26 +0000

    Removed concurrency benchmark related files

--- src/main/java/com/github/tiniyield/sequences/benchmarks/concurrency/math/sine/Sine.java
+++
@@ -1,7 +0,0 @@
-package com.github.tiniyield.sequences.benchmarks.concurrency.math.sine;
-
-public class Sine {
-    static double slowSin(int value) {
-        return FastMathCalc.slowSin(value, null);
-    }
-}
