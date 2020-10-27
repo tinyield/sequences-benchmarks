71f2f700e2c2c8c5bf4e968cdaa38c31402414ae
dpoeira <dpoeira@infinera.com>
2020-10-27 15:54:55 +0000

    AllMatchBenchmark now provides it's own data

--- src/main/java/com/github/tiniyield/sequences/benchmarks/all/match/AllMatchBenchmark.java
+++ src/main/java/com/github/tiniyield/sequences/benchmarks/all/match/AllMatchBenchmark.java
@@ -1,14 +1,18 @@
 package com.github.tiniyield.sequences.benchmarks.all.match;
 
 import com.github.tiniyield.sequences.benchmarks.AbstractSequenceOperationsBenchmark;
-import com.github.tiniyield.sequences.benchmarks.operations.data.providers.number.EvenSequenceDataProvider;
+import kotlin.collections.ArraysKt;
+import one.util.streamex.StreamEx;
+import org.jayield.Query;
+import org.jooq.lambda.Seq;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import static com.github.tiniyield.sequences.benchmarks.operations.common.SequenceBenchmarkConstants.EVEN;
 import static com.github.tiniyield.sequences.benchmarks.operations.common.SequenceBenchmarkUtils.assertEveryEvenValidity;
-import static com.github.tiniyield.sequences.benchmarks.operations.common.SequenceBenchmarkUtils.getEvenDataProvider;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -17,41 +21,47 @@ public class AllMatchBenchmark extends AbstractSequenceOperationsBenchmark {
 
     @Param({"10000"})
     public int COLLECTION_SIZE;
-    private EvenSequenceDataProvider provider;
+    private Integer[] data;
+
+    public Integer[] getAllEvenArray() {
+        Integer[] numbers = new Integer[COLLECTION_SIZE];
+        Arrays.fill(numbers, EVEN);
+        return numbers;
+    }
 
     @Setup
     public void setup() {
         super.init();
-        provider = getEvenDataProvider(COLLECTION_SIZE);
+        data = getAllEvenArray();
         assertEveryEvenValidity(getStream(), getStreamEx(), getQuery(), getJool(), getVavr());
     }
 
     private boolean getStream() {
-        return stream.isEveryEven(provider.asStream());
+        return stream.isEveryEven(Arrays.stream(data));
     }
 
     private boolean getStreamEx() {
-        return streamEx.isEveryEven(provider.asStreamEx());
+        return streamEx.isEveryEven(StreamEx.of(data));
     }
 
     private boolean getQuery() {
-        return query.isEveryEven(provider.asQuery());
+        return query.isEveryEven(Query.of(data));
     }
 
     private boolean getJool() {
-        return jool.isEveryEven(provider.asSeq());
+        return jool.isEveryEven(Seq.of(data));
     }
 
     private boolean getVavr() {
-        return vavr.isEveryEven(provider.asVavrStream());
+        return vavr.isEveryEven(io.vavr.collection.Stream.of(data));
     }
 
     private boolean getKotlin() {
-        return kotlin.isEveryEven(provider.asSequence());
+        return kotlin.isEveryEven(ArraysKt.asSequence(data));
     }
 
     private boolean getJKotlin() {
-        return jkotlin.isEveryEven(provider.asSequence());
+        return jkotlin.isEveryEven(ArraysKt.asSequence(data));
     }
 
     @Benchmark
